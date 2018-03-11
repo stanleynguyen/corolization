ccb86d673d328fa957f3b1e77410b00d1dbb4f3b
Stanley Nguyen <hung.ngn.the@gmail.com>
2018-03-12 01:21:34 +0800

    Add saving of trained model

---
+++ train.py
@@ -0,0 +1,72 @@
+import torch
+import torch.nn as nn
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+from corolization import ResidualEncoder
+import torch.nn.functional as F
+import numpy as np
+from skimage.color import rgb2yuv, yuv2rgb
+import matplotlib.pyplot as plt
+
+train_dataset = dsets.CIFAR10(root='./data/',
+                              train=True,
+                              #                                transform=transforms.ToTensor(),
+                              download=True)
+
+# plt.imshow(yuv2rgb(rgb2yuv(np.array(train_dataset[0][0]))))
+# plt.show()
+
+test_dataset = dsets.CIFAR10(root='./data/',
+                             #                               transform=transforms.ToTensor(),
+                             train=False)
+
+
+def massage_dset(dset):
+    rtv = []
+    for i, (image, _) in enumerate(dset):
+        img = rgb2yuv(image)
+        rtv.append((np.array(img[:, :, 0:1]).reshape(
+            1, 32, 32), np.array(img[:, :, 1:]).reshape(2, 32, 32)))
+
+    return rtv
+
+
+train_dataset = massage_dset(train_dataset)
+test_dataset = massage_dset(test_dataset)
+
+batch_size = 100
+num_epochs = 3
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size,
+                                           shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size,
+                                          shuffle=False)
+
+encoder = ResidualEncoder(32)
+criterion = nn.MSELoss()
+learning_rate = 0.001
+optimizer = torch.optim.SGD(encoder.parameters(), lr=learning_rate)
+
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        images = Variable(torch.stack(images))
+#         images = Variable(images)
+        labels = Variable(labels)
+
+        # Forward + Backward + Optimize
+        optimizer.zero_grad()
+        outputs = encoder.double()(images)
+#         outputs = encoder(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        print('Iter %d/%d' % (i+1, len(train_dataset)//batch_size))
+        if (i+1) % 100 == 0:
+            print('Epoch [%d/%d], Iter [%d/%d] Loss: %.4f'
+                  % (epoch+1, num_epochs, i+1, len(train_dataset)//batch_size, loss.data[0]))
+            torch.save(encoder.state_dict(), 'residual_encoder.pkl')
