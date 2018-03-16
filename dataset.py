7e207fd7f22d7959558e0321b05d53b61ae70411
Stanley Nguyen <hung.ngn.the@gmail.com>
2018-03-16 09:52:44 +0800

    Custome Datasets

--- dataset.py
+++ dataset.py
@@ -2,8 +2,11 @@
 
 import torch
 from torch.utils.data import Dataset
-from skimage.color import rgb2yuv
+from skimage.color import rgb2yuv, rgb2lab
+from skimage.io import imread
 import torchvision.datasets as dsets
+from os import listdir
+from os.path import join, isfile
 
 
 class CIFAR10ForResEncoder(Dataset):
@@ -20,6 +23,40 @@ class CIFAR10ForResEncoder(Dataset):
         bwimg = data[:, :, 0:1].transpose(2, 0, 1)
         bwimg = torch.from_numpy(bwimg).float()
         label = data[:, :, 1:].transpose(2, 0, 1)
+        label = torch.from_numpy(label).float()
+
+        if self.transform is not None:
+            bwimg = self.transform(bwimg)
+
+        return (bwimg, label)
+
+
+class CustomImages(Dataset):
+    def __init__(self, root, train=True, color_space='yub', transform=None):
+        """
+            color_space: 'yub' or 'lab'
+        """
+        self.root_dir = root
+        self.root_dir += join(self.root_dir,
+                              '/train') if train else join(self.root_dir, '/test')
+        self.filenames = [f for f in listdir(
+            self.root_dir) if isfile(join(self.root_dir, f))]
+        self.color_space = color_space
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.filenames)
+
+    def __getitem__(self, idx):
+        img = imread(join(self.root_dir, self.filenames[idx]))
+        if self.color_space == 'lab':
+            img = rgb2lab(img)
+        else:
+            img = rgb2yuv(img)
+
+        bwimg = img[:, :, 0:1].transpose(2, 0, 1)
+        bwimg = torch.from_numpy(bwimg).float()
+        label = img[:, :, 1:].transpose(2, 0, 1)
         label = torch.from_numpy(label).float()
 
         if self.transform is not None:
