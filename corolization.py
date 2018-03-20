feb3cf8f7745623b4322093f54c7ae29b324dc05
Stanley Nguyen <hung.ngn.the@gmail.com>
2018-03-20 23:29:46 +0800

    Concat instead of summing

--- corolization.py
+++ corolization.py
@@ -46,17 +46,17 @@ class ResidualEncoder(nn.Module):
         self.enc_bn1 = nn.BatchNorm2d(512)
         self.enc_deconv1 = nn.ConvTranspose2d(
             256, 256, 3, padding=1, stride=2, output_padding=1)
-        self.enc_conv2 = nn.Conv2d(256, 128, 3, padding=1)
+        self.enc_conv2 = nn.Conv2d(512, 128, 3, padding=1)
         self.enc_bn2 = nn.BatchNorm2d(256)
         self.enc_deconv2 = nn.ConvTranspose2d(
             128, 128, 3, padding=1, stride=2, output_padding=1)
-        self.enc_conv3 = nn.Conv2d(128, 64, 3, padding=1)
+        self.enc_conv3 = nn.Conv2d(256, 64, 3, padding=1)
         self.enc_bn3 = nn.BatchNorm2d(128)
         self.enc_deconv3 = nn.ConvTranspose2d(
             64, 64, 3, padding=1, stride=2, output_padding=1)
-        self.enc_conv4 = nn.Conv2d(64, 3, 3, padding=1)
+        self.enc_conv4 = nn.Conv2d(128, 3, 3, padding=1)
         self.enc_bn4 = nn.BatchNorm2d(64)
-        self.enc_conv5 = nn.Conv2d(3, 3, 3, padding=1)
+        self.enc_conv5 = nn.Conv2d(6, 3, 3, padding=1)
         self.enc_bn5 = nn.BatchNorm2d(3)
 
         self.final_conv = nn.Conv2d(3, 2, 3, padding=1)
@@ -75,18 +75,18 @@ class ResidualEncoder(nn.Module):
         out = self.enc_conv1(out)
         enc_out1 = self.enc_deconv1(out)
         out = self.enc_bn2(vgg_out4)
-        out += enc_out1
+        out = torch.cat((out, enc_out1), 1)
         out = self.enc_conv2(out)
         enc_out2 = self.enc_deconv2(out)
         out = self.enc_bn3(vgg_out3)
-        out += enc_out2
+        out = torch.cat((out, enc_out2), 1)
         out = self.enc_conv3(out)
         enc_out3 = self.enc_deconv3(out)
         out = self.enc_bn4(vgg_out2)
-        out += enc_out3
+        out = torch.cat((out, enc_out3), 1)
         enc_out4 = self.enc_conv4(out)
         out = self.enc_bn5(vgg_out1)
-        out += enc_out4
+        out = torch.cat((out, enc_out4), 1)
         enc_out5 = self.enc_conv5(out)
 
         final_out = self.final_conv(enc_out5)
