a14008a6b2cadbc83f896a54de1f6fe74f1803a6
Vivek Kalyan <vivekkalyansk@gmail.com>
2018-03-30 13:29:50 +0800

    Add script to resize photos
    Requires imagemagick

--- get_dataset.sh
+++ get_dataset.sh
@@ -2,5 +2,7 @@ echo "Downloading SUN dataset..."
 wget -c http://groups.csail.mit.edu/vision/SUN/releases/SUN2012.tar.gz
 echo "Extracting..."
 tar xzf SUN2012.tar.gz
+echo "Resizing images..."
+find . -name "*.jpg" | xargs mogrify -resize 256x256!
 echo "Done!"
 
