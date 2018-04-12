ddf78745a1ea326a5c668591c5680ee0ed410595
bowsplinter <vivekkalyansk@gmail.com>
2018-04-12 21:54:40 +0800

    Add function to create prior colour probabilties

--- calc_weights.py
+++ calc_weights.py
@@ -4,6 +4,7 @@ import sklearn.neighbors as nn
 from math import sqrt, pi
 import os
 import torch
+import pdb
 
 # calculate the weight for each bin based on empirical probability, for class rebalancing
 # only needs to be run once
@@ -22,17 +23,43 @@ def cal_emp_weights(dset, bins_num=313, sigma=5, lamda=0.5):
         bins_prob[torch.from_numpy(indexes).view(-1)] += 1
     bins_sum = bins_prob.sum()
     bins_prob /= bins_sum
+    pdb.set_trace()
 
-    bins_idx_arr = np.arange(bins_num)
-
-    for i, prob in enumerate(bins_prob):
-        bins_prob[i] = (prob * (np.power(i - bins_idx_arr, 2) /
-                                (sigma * sigma)) / (sqrt(2 * pi) * sigma)).sum()
-
+    # bins_idx_arr = np.arange(bins_num)
+    # bins_prob_gauss = torch.zeros(bins_num)
+    # for i, prob in enumerate(bins_prob):
+    #     bins_prob_gauss[i] = (prob * (np.power(i - bins_idx_arr, 2) /
+    #                             (sigma * sigma)) / (sqrt(2 * pi) * sigma)).sum()
+    # pdb.set_trace()
     w = 1/((1 - lamda) * bins_prob + lamda / bins_num)
     w /= ((bins_prob * w).sum())
-    torch.save(w, 'static/weights')
+    torch.save(w, 'static/weights_test')
     return w
 
-entire_dataset = dataset.CustomImages('./SUN2012', train=False, test_size=0.999, val_size=0)
-cal_emp_weights(entire_dataset, 313)
+# entire_dataset = dataset.CustomImages('./SUN2012', train=False, test_size=0.01, val_size=0)
+# cal_emp_weights(entire_dataset, 313)
+
+def prior():
+    priorFile = 'static/prior_probs.npy'
+    gamma = 0.5
+
+    prior_probs = np.load(priorFile)
+    pdb.set_trace()
+
+    # define uniform probability
+    uni_probs = np.zeros_like(prior_probs)
+    uni_probs[prior_probs!=0] = 1.
+    uni_probs = uni_probs/np.sum(uni_probs)
+
+    # convex combination of empirical prior and uniform distribution
+    prior_mix = (1-gamma)*prior_probs + gamma*uni_probs
+
+    # set prior factor
+    prior_factor = 1/prior_mix
+    prior_factor = prior_factor/np.sum(prior_probs*prior_factor) # re-normalize
+
+    # implied empirical prior
+    implied_prior = prior_probs*prior_factor
+    implied_prior = implied_prior/np.sum(implied_prior) # re-normalize
+
+prior()
